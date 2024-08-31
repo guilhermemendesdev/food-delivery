36bf54b8ef4bd8a181f33bc558548aec8d62a67f
Guilherme <guilhermemendesdev@gmail.com>
2024-08-31 07:45:10 -0300

    feat: configure screen to user logged

--- clients/user-ui/src/components/ProfileDropDown.tsx
+++ clients/user-ui/src/components/ProfileDropDown.tsx
@@ -1,52 +1,74 @@
-'use client'
-import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/react"
-import { useState } from "react"
-import { CgProfile } from "react-icons/cg"
-import AuthScreen from "../screens/AuthScreen"
+'use client';
+import {
+  Avatar,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from '@nextui-org/react';
+import { useEffect, useState } from 'react';
+import { CgProfile } from 'react-icons/cg';
+import AuthScreen from '../screens/AuthScreen';
+import useUser from '../hooks/useUser';
+import toast from 'react-hot-toast';
+import Cookies from 'js-cookie';
 
 const ProfileDropDown = () => {
-    const [signedIn, setsignedIn] = useState(false)
-    const [open, setOpen] = useState(false)
+  const [signedIn, setsignedIn] = useState(false);
+  const [open, setOpen] = useState(false);
+  const { user, loading } = useUser();
 
-return (
+  useEffect(() => {
+    if (!loading) {
+      console.log(user);
+      setsignedIn(!!user);
+    }
+  }, [loading, user, open]);
+
+  const logoutHandler = () => {
+    Cookies.remove('access_token');
+    Cookies.remove('refresh_token');
+    toast.success('Log Out successful!');
+    window.location.reload();
+  };
+
+  console.log(signedIn);
+
+  return (
     <div className="flex items-center gap-4">
-       {
-        signedIn ? (
-             <Dropdown placement="bottom-end">
-            <DropdownTrigger>
-                <Avatar 
-                as="button"
-                className="transition-transform"
-                src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-                />
-            </DropdownTrigger>
-            <DropdownMenu aria-label="Profile Actions" variant="flat">
-                <DropdownItem key="profile" className="h-14 gap-2">
-                    <p className="font-semibold">Signed in as</p>
-                    <p className="font-semibold">support@becodemy.com</p>
-                </DropdownItem>
-                <DropdownItem key="settings">My Profile</DropdownItem>
-                <DropdownItem key="all_orders">All Orders</DropdownItem>
-                <DropdownItem key="team_settings">Apply for seller account</DropdownItem>
-                <DropdownItem key="logout" color="danger">
-                    Log Out
-                </DropdownItem>
-            </DropdownMenu>
-        </Dropdown>
-        ) : (
-            <CgProfile
-            className="text-2xl cursor-pointer"
-            onClick={() => setOpen(!open)}
+      {signedIn ? (
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              as="button"
+              className="transition-transform"
+              src={user?.avatar?.url}
             />
-        )
-       }
-       {
-        open && (
-            <AuthScreen />
-        )
-       }
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">{user.email}</p>
+            </DropdownItem>
+            <DropdownItem key="settings">My Profile</DropdownItem>
+            <DropdownItem key="all_orders">All Orders</DropdownItem>
+            <DropdownItem key="team_settings">
+              Apply for seller account
+            </DropdownItem>
+            <DropdownItem key="logout" color="danger" onClick={logoutHandler}>
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      ) : (
+        <CgProfile
+          className="text-2xl cursor-pointer"
+          onClick={() => setOpen(!open)}
+        />
+      )}
+      {open && <AuthScreen setOpen={setOpen} />}
     </div>
-)
-}
+  );
+};
 
-export default ProfileDropDown
+export default ProfileDropDown;
