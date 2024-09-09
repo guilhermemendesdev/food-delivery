157a0b7ed6598d7b99ac78fc705c7822335385fc
Guilherme <guilhermemendesdev@gmail.com>
2024-09-09 07:34:06 -0300

    feat: adiciona modulo Hero a tela inicial

--- clients/user-ui/src/screens/HomeScreen.tsx
+++ clients/user-ui/src/screens/HomeScreen.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import Header from "../components/Layout/Header";
+import React from 'react';
+import Header from '../components/Layout/Header';
+import Hero from '../shared/views/Root/Hero';
 
 const HomeScreen = () => {
   return (
-  <div>
-    <Header/>
-  </div>
-);
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
 };
 
 export default HomeScreen;
