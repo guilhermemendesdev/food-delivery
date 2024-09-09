ee8c4a727bf7ada8f81258775ab3eee1834f6f27
Guilherme <guilhermemendesdev@gmail.com>
2024-09-09 07:32:09 -0300

    feat: altera cor do header

--- clients/user-ui/src/components/Layout/Header.tsx
+++ clients/user-ui/src/components/Layout/Header.tsx
@@ -1,19 +1,17 @@
-import styles from '@/src/utils/style'
-import NavItems from "../NavItems"
-import ProfileDropDown from "../ProfileDropDown"
+import styles from '../../utils/style';
+import NavItems from '../NavItems';
+import ProfileDropDown from '../ProfileDropDown';
 
 const Header = () => {
   return (
-    <header className="w-full bg-[#0f1524]">
+    <header className="w-full bg-[#0A0713]">
       <div className="w-[90%] m-auto h-[80px] flex items-center justify-between">
-        <h1 className={`${styles.logo}`}>
-          Monta Massa
-        </h1>
+        <h1 className={`${styles.logo}`}>Monta Massa</h1>
         <NavItems />
         <ProfileDropDown />
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
